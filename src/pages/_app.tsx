565f9049fc70f600448dcdea63d7740b957d2ec8
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-25 18:07:42 -0300

    instalando chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,37 @@
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+// Declarando o que vamos extender 
+const colors = {
+  barber:{
+    900: '#12131b',
+    400: '#1b1v29',
+    100: '#c6c6c6'
+  },
+
+  button:{
+    cta: '#fba931',
+    default: '#FFF',
+    gray: '#DFDFDF',
+    danger: '#FF4040'
+  },
+
+  orange: {
+    900: '#fba931'
+  }
+}
+
+//extendendo
+const theme = extendTheme({ colors })
 
 function MyApp ({Component, pageProps}: AppProps ){
-  return <Component {...pageProps} />
+  return(
+                    //passando o tema para o chakra
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
