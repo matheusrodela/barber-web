a792d8f406cb32f312e3e113c0629ab9b1df7020
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-28 15:33:15 -0300

    fazendo login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useState } from "react"
-import { destroyCookie } from 'nookies'
+import { destroyCookie,setCookie } from 'nookies'
 import Router from 'next/router'
 
 import { api } from "../services/apiClient";
@@ -60,7 +60,25 @@ export function AuthProvider({ children }: AuthProviderProps){
                 password,
             })
 
-            console.log(response.data);
+            const { id, name, token, subscriptions, endereco } = response.data;
+
+            setCookie(undefined, '@barber.token', token, {
+                maxAge: 60 * 60 * 24 * 30, //expirar em 1 mes
+                path: '/'
+            })
+
+            setUser({
+                id,
+                name,
+                email,
+                endereco,
+                subscriptions
+            })
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            Router.push('/dashboard');
+
         }catch(err){
             console.log("ERRO AO ENTRAR", err)
         }
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Dashboard(){
+    return(
+        <>
+        <Head>
+            <title>
+                BarberPro - Minha barbearia
+            </title>
+        </Head>
+
+        <Flex>
+            <Text>Bem vindo ao dashboard</Text>
+        </Flex>
+        </>
+    )
+}
