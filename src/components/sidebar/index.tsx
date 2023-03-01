a1a0a9153492b3ac487202fdec9f80f3c1c62602
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-28 23:02:14 -0300

    sidebar desk e mobile

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -49,7 +49,23 @@ export function Sidebar({ children }: { children: ReactNode }){
                 onClose={() => onClose}
                 display={{ base: 'none', md: "block" }}
             />
-            <Box>
+
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full"
+                onClose={onClose}
+            >
+                <DrawerContent color={"white"}>
+                    <SidebarContent onClose={() => onClose()} />
+                </DrawerContent>
+            </Drawer>
+
+            <MobileNav display={{base: 'flex', md: 'none'}} onOpen={onOpen} />
+            <Box ml={{base:0, md:60 }} p={"4"}>
                 {children}
             </Box>
         </Box>
@@ -129,4 +145,42 @@ return(
     </Flex>
 </Link>
 )
+}
+
+
+//  --- MOBILE ---
+interface MobileProps extends FlexProps{
+    onOpen: () => void;
+}
+
+const MobileNav = ({onOpen, ...rest}: MobileProps) => {
+    return(
+        <Flex
+        ml={{base: 0, md: 60}}
+        px={{base: 4, md: 24}}
+        height="20"
+        alignItems={"center"}
+        bg={useColorModeValue('white', 'gray.900')}
+        borderBottomWidth="1px"
+        borderBottomColor={useColorModeValue('gray.600', 'gray.700')}
+        justifyContent="flex-start"
+        backgroundColor={"gray.900"}
+        {...rest}
+        >
+            <IconButton
+                variant={'outline'}
+                borderColor={useColorModeValue('gray.600', 'gray.700')}
+                onClick={onOpen}
+                aria-label="Abrir menu"
+                _hover={{bg:"gray.700"}}
+    
+                icon={<FiMenu/>}
+            />
+
+            <Flex flexDirection={"row"}>
+                <Text ml={"4"} fontSize="2xl" fontFamily="monospace" fontWeight={"bold"}>Barber</Text>
+                <Text fontSize="2xl" fontFamily="monospace" fontWeight={"bold"} color="button.cta">PRO</Text>
+            </Flex>
+        </Flex>
+    )
 }
